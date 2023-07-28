c1db0de56a8a19ab0d19693da4d0a3f84e586706
Abraham Putra P <abrahamprakasa@gmail.com>
2023-07-28 14:47:49 +0700

    cosmetic: minor haul login page

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-    include('view/header.php');
-    if (isset($_SESSION["email"]) && $_SESSION["email"]) {
-        header("Location: dashboard.php");
-        die();
-    }
+include('view/header.php');
+if (isset($_SESSION["email"]) && $_SESSION["email"]) {
+    header("Location: dashboard.php");
+    die();
+}
 ?>
 
 <div class="container col-lg-6 my-5 center">
@@ -12,24 +12,24 @@
         <p class="fs-5">Silakan login terlebih dahulu</p>
         <form class="d-flex flex-column" role="search" method="POST" action="api/login.php">
             <div class="my-1">
-                <input class="form-control me-2 my-1" type="text" placeholder="Email" name="email" aria-label="Search" required autocomplete="off">
+                <input class="form-control me-2 my-1" type="email" placeholder="Email" name="email" aria-label="Search" required autocomplete="off">
                 <input class="form-control me-2" type="password" placeholder="Password" name="password" aria-label="Search" required autocomplete="off">
+                <?php if (isset($_SESSION["error_message"])) : ?>
+                    <div class="alert alert-danger mt-2" role="alert">
+                        <?php
+                        echo $_SESSION["error_message"];
+                        unset($_SESSION["error_message"]);
+                        ?>
+                    </div>
+                <?php endif ?>
             </div>
             <div class="text-center">
                 <button class="btn btn-outline-success w-50" type="submit">Login</button>
             </div>
         </form>
-        <?php if (isset($_SESSION["error_message"])) : ?>
-            <div class="alert alert-danger" role="alert">
-                <?php
-                echo $_SESSION["error_message"];
-                unset($_SESSION["error_message"]);
-                ?>
-            </div>
-        <?php endif ?>
     </div>
 </div>
 
 <?php
-    include('view/footer.php');
+include('view/footer.php');
 ?>
